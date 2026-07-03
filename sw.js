71cacdb682887c0f8a494f69ec37032212968d19
FlahMusic <flahobino@gmail.com>
2026-07-03 02:17:28 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+const C='slimehedron-v1';
+const FILES=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(FILES)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))));});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(n=>{
+  if(e.request.method==='GET'&&n.ok&&new URL(e.request.url).origin===location.origin)caches.open(C).then(c=>c.put(e.request,n.clone()));
+  return n;}).catch(()=>caches.match('./index.html'))));});
