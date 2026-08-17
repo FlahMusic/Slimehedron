ffea6fdf4b883669124f31ffc40e976c806e8e3a
FlahMusic <flahobino@gmail.com>
2026-08-17 16:36:51 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C='slimehedron-v9'; // bump each deploy
-const FILES=['./','./index.html','./learn.js','./landing.html','./manifest.json','./icon-192.png','./icon-512.png','./slimehedron-mascot.png','./bg.jpg','./slimelogo.png'];
+const C='slimehedron-v10'; // bump each deploy
+const FILES=['./','./index.html','./learn.js','./landing.html','./manifest.json','./icon-192.png','./icon-512.png','./slimehedron-mascot.png','./bg-desktop.jpg','./bg-mobile.jpg','./slimelogo.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(FILES)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{
